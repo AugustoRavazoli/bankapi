c188893623d91826183381dbacdf085d96ff045c
Augusto Ravazoli <augustoravazoli@gmail.com>
2023-04-22 11:24:06 -0300

    change: override toString

--- src/main/java/io/github/augustoravazoli/bankapi/transaction/TransactionType.java
+++ src/main/java/io/github/augustoravazoli/bankapi/transaction/TransactionType.java
@@ -1,9 +1,17 @@
 package io.github.augustoravazoli.bankapi.transaction;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 enum TransactionType {
 
   TRANSFERATION,
   DEPOSIT,
   WITHDRAWAL;
+
+  @JsonValue
+  @Override
+  public String toString() {
+    return super.toString().toLowerCase();
+  }
 
 }
